d66c7790e3ec2b192eaf203b86d8f7c6df89ea1e
Raiyan109 <kraiyan109@gmail.com>
2024-07-09 14:19:58 +0600

    error files, product interface, model and controller

---
+++ src/app/modules/products/product.interface.ts
@@ -0,0 +1,9 @@
+
+export type TProduct = {
+    image: string;
+    title: string;
+    price: number;
+    rating: string;
+    category: [string];
+    availableInStock: number;
+} 
---
+++ src/app/modules/products/product.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { TProduct } from "./product.interface";
+
+
+
+const productSchema = new Schema<TProduct>({
+    image: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: [String],
+        required: true
+    },
+    availableInStock: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
+
+export const ProductModel = model<TProduct>('Product', productSchema)
